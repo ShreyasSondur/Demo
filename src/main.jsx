85b6bf5189387bc007345304b2cbc3cfef422e94
ShreyasSondur <Shreyas.u.sondur@gmail.com>
2025-02-05 23:25:26 +0530

    changed header

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ createRoot(document.getElementById('root')).render(
     <h1>First Time Finally T-T</h1>
     <h2>My name is shreyas</h2>
     <h2>Srushti sponsor</h2>
+    <h2>TY AKKA 😘</h2>
     <App />
   </StrictMode>,
 )
