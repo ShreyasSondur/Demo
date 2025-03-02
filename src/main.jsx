c461f137c39c6080d09ce73305deba22ced54d28
ShreyasSondur <Shreyas.u.sondur@gmail.com>
2025-03-03 01:25:04 +0530

    changed

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import App from './components/App'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <h1>First Time Finally T-T</h1>
+    <h2>Made for learning , not in working condition</h2>
     <App />
   </StrictMode>,
 )
